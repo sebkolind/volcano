fcd1fc36434dc643230e59a3ea7ce803d9adcd9f
Sebastian Kolind Sørensen <sks1993@gmail.com>
2017-08-25 16:07:14 +0200

    Add $nested_path to parse_markdown()

--- core/markdown.php
+++ core/markdown.php
@@ -3,11 +3,24 @@
 /**
  * @desc - parse markdown
  * @param string $filename - the filename
+ * @param string $nested_path - nested path inside /site/pages
  */
-function parse_markdown($filename = null) {
+function parse_markdown($filename = false, $nested_path = false) {
 	$filepath = !$filename
 				? PAGES . '/default.md'
 				: PAGES . '/' . $filename . '.md';
+
+	if ($nested_path) {
+		if (!$filename) {
+			die('When using nested path (second argument) you have to give filename as first argument');
+		}
+
+		$filepath = PAGES . $nested_path . '/' . $filename . '.md';
+	} else {
+		$filepath = !$filename
+			? PAGES . '/default.md'
+			: PAGES . '/' . $filename . '.md';
+	}
 
 	if (!file_exists($filepath)) {
 		$filename = USE_404 && file_exists(PAGES . '/404.md') ? '404' : 'default';
