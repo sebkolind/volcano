6b4cff41a6c9f0b75c1e6e43569ea29558019669
Sebastian Kolind Sørensen <sks1993@gmail.com>
2017-09-20 19:19:31 +0200

    Add constants again - sorry, wrong ones.

--- setup.php
+++ setup.php
@@ -25,3 +25,13 @@ define('USE_HOME_TEMPLATE', true);
  * Should be false when your site is live
  */
 define('IS_DEV', false);
+
+/**
+ * @desc - use minified CSS or not
+ */
+define('USE_MINIFIED_CSS', true);
+
+/**
+ * @desc - use minified JS or not
+ */
+define('USE_MINIFIED_JS', true);
