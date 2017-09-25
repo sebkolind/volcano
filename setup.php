ebab9e92aa903d7cc942ff807b0d9b9d342086da
Sebastian Kolind Sørensen <sks1993@gmail.com>
2017-09-25 16:33:28 +0200

    Add defaults

--- setup.php
+++ setup.php
@@ -8,34 +8,40 @@
 
 /**
  * @desc - use 404 page or not
- * 	if "false" redirect to default.md
- * 	default: true
+ * if "false" redirect to default.md
+ * default: true
  */
 define('USE_404', true);
 
 /**
  * @desc - use index.php or template as home
- * 	if "true" template has to be: site/templates/home.php
+ * if "true" template has to be: site/templates/home.php
+ * default: true
  */
 define('USE_HOME_TEMPLATE', true);
 
 /**
  * @desc - true: show errors
  * 		   false - don't show errors.
+ * default: false
+ *
  * Should be false when your site is live
+ *
  * By setting to true, you'll get error messages and
  * your CSS and JS will not cache, which makes it easier
  * for you to make a theme for instance. If set to false, you have to
  * hard refresh the browser everytime you change something i .css or .js files.
  */
-define('IS_DEV', true);
+define('IS_DEV', false);
 
 /**
  * @desc - use minified CSS or not
+ * default: true
  */
 define('USE_MINIFIED_CSS', true);
 
 /**
  * @desc - use minified JS or not
+ * default: true
  */
 define('USE_MINIFIED_JS', true);
