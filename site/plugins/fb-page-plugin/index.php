82464be589994378e127f25c79fd3f1b1b857812
Sebastian Kolind Sørensen <sks1993@gmail.com>
2017-09-25 16:46:13 +0200

    Use ternary

--- site/plugins/fb-page-plugin/index.php
+++ site/plugins/fb-page-plugin/index.php
@@ -22,22 +22,21 @@ function fbPagePlugin($options = false) {
 	 * allows user to actually set attributes.
 	 * If no $options given, use defaults
 	 */
-	if ($options) {
-		$options = array_merge($defaults, $options);
-	} else {
-		$options = $defaults;
-	}
+	$options = $options 
+		? array_merge($defaults, $options) 
+		: $defaults;
 
 	?>
-	<div
-		class="fb-page"
-		data-href="<?php echo $options['href']; ?>"
-		data-width="<?php echo $options['width']; ?>"
-		data-height="<?php echo $options['height']; ?>"
-		data-hide-cover="<?php echo $options['hide_cover']; ?>"
-		data-show-facepile="<?php echo $options['show_facepile']; ?>"
-		data-hide-cta="<?php echo $options['hide_cta']; ?>"
-		data-small-header="<?php echo $options['small_header']; ?>"
-		data-adapt-container-width="<?php echo $options['adapt_container_width']; ?>"></div>
+		<div
+			class="fb-page"
+			data-href="<?php echo $options['href']; ?>"
+			data-width="<?php echo $options['width']; ?>"
+			data-height="<?php echo $options['height']; ?>"
+			data-hide-cover="<?php echo $options['hide_cover']; ?>"
+			data-show-facepile="<?php echo $options['show_facepile']; ?>"
+			data-hide-cta="<?php echo $options['hide_cta']; ?>"
+			data-small-header="<?php echo $options['small_header']; ?>"
+			data-adapt-container-width="<?php echo $options['adapt_container_width']; ?>">
+		</div>
 	<?
 }
